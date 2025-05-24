9ef1f5292168a1216624df382766fa5f35ecd627
stivenBermeo <bermeopalomares3@gmail.com>
2025-05-24 08:27:26 -0500

    chore: insert owners

--- services/owner_service/src/index.ts
+++ services/owner_service/src/index.ts
@@ -1,10 +1,30 @@
 import express, { Request, Response } from 'express';
+import { MongoClient } from 'mongodb';
+import 'dotenv/config'
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello, TypeScript Express!');
+app.use(express.json())
+
+app.get('/', async (_req: Request, res: Response) => {
+  const connectionUri = process.env.MONGODB_QUERY_STRING;
+  const client = new MongoClient(connectionUri ?? '')
+  await client.connect()
+  const dbo = client.db('vet_clinic')
+  const owners = await dbo.collection('owner').find({}).toArray()
+  res.send({ owners });
+});
+
+app.post('/', async (req: Request, res: Response) => {
+  const connectionUri = process.env.MONGODB_QUERY_STRING;
+  const client = new MongoClient(connectionUri ?? '')
+  await client.connect()
+  const dbo = client.db('vet_clinic')
+
+  await dbo.collection('owner').insertOne(req.body)
+  const data = req.body
+  res.send({ data });
 });
 
 app.listen(port, () => {
